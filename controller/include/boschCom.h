e2a973806fe33b7927078320924dc153d0f9b220
Tim Goll <tim@timgoll.de>
2016-11-18 23:45:42 +0100

    added filestructure 2

---
+++ controller/include/boschCom.h
@@ -0,0 +1,16 @@
+//
+// Created by Tim Goll on 17.11.16.
+//
+
+#ifndef V0_0_1_BOSCHCOM_H
+#define V0_0_1_BOSCHCOM_H
+
+namespace communication {
+    class BoschCom {
+    public:
+        BoschCom(); //Defaultconstructor
+    private:
+    };
+}
+
+#endif //V0_0_1_BOSCHCOM_H
---
+++ controller/include/mfcCtrl.h
@@ -0,0 +1,8 @@
+//
+// Created by Tim Goll on 17.11.16.
+//
+
+#ifndef V0_0_1_MFCCTRL_H
+#define V0_0_1_MFCCTRL_H
+
+#endif //V0_0_1_MFCCTRL_H
